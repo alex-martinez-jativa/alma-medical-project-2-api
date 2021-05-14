c75cc863aaf4256912cadaf8418d0c97abbf7091
Alex Martinez <a.martinez@kubikdata.com>
2021-05-14 15:18:45 +0200

    feat: simple node api to serve data files

---
+++ index.js
@@ -0,0 +1,22 @@
+const PORT = 3001;
+const fs = require('fs');
+const data = fs.readFileSync('./data/boat_ramps.geojson.json');
+   
+const rampsData = JSON.parse(data);
+const express = require("express");
+const app = express();
+   
+const cors=require('cors');
+    
+app.listen(PORT, 
+    () => console.log("Server Starts"));
+    
+app.use(express.static('public'));
+app.use(cors());
+  
+app.get('/ramps', alldata);
+   
+function alldata(req, res) {
+    res.send(rampsData);
+}
+  
